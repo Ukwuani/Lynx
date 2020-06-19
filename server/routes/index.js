daf5b2ca1144054b15ea8b49ddd11f940fdb9b1a
ukwuani <ukwuanibarnabas@gmail.com>
2020-06-19 01:35:00 +0100

    style(modify): changed the routing style

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,20 +2,8 @@
 let path 	= require("path");
 const router = require('express').Router()
 
-
-module.exports = router
-
-module.exports = (app, db, atAPI) => {
-
-	
-	app.get("/", (req, res) => {
-		console.log("stage 1")
-	
-		res.sendFile(path.join(__dirname+'/../www/index.html'));
-	});
-
 	// Handle Auth routes
-	 require("./auth")(router);
+	require("./auth")(router);
 
 	//Handle Africas Talking API
 	require("./africastalk")(app, db, atAPI);
@@ -23,8 +11,7 @@ module.exports = (app, db, atAPI) => {
 	// Handle Auth routes
 	require("./account")(router);
 
-
-
-	// // Handle errors
+	// Handle errors
 	// require("./errors")(app, db);	
-};
+
+module.exports = router
